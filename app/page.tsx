d4630688a0ba22ef0543b21f840a9267803b770a
Diego Roccia <diego.roccia@gmail.com>
2025-01-12 21:52:16 +0100

    Sun Jan 12 09:52:16 PM CET 2025

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import styles from "./page.module.css";
 import Footer from "./footer";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <div className={styles.logoContainer}>
+          <Image
+            alt="Logo"
+            src="/logo.png"
+            width={100}
+            height={100}
+            className={styles.logo}
+          />
+        </div>
         <h1 className={styles.title}>Diego Roccia</h1>
       </main>
       <Footer />
