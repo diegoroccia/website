982f6ec7d5ec02e194be70523c19a8ce79dfe53e
Diego Roccia <diego.roccia@gmail.com>
2025-01-12 22:03:57 +0100

    Sun Jan 12 10:03:57 PM CET 2025

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,14 @@
 import styles from "./page.module.css";
 import Footer from "./footer";
-import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloud } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.logoContainer}>
-          <Image
-            alt="Logo"
-            src="/logo.png"
-            width={100}
-            height={100}
-            className={styles.logo}
-          />
+          <FontAwesomeIcon icon={faCloud} />
         </div>
         <h1 className={styles.title}>Diego Roccia</h1>
       </main>
