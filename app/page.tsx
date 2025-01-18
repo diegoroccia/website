a6f8808a41dcb922c329e806dbe2b652fdcd1a1e
Diego Roccia <diego.roccia@gmail.com>
2025-01-18 13:40:05 +0100

    Sat Jan 18 01:40:05 PM CET 2025
    Signed-off-by: Diego Roccia <diego.roccia@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-      <main className="container mx-auto text-slate-400 bg-slate-900 min-h-screen flex items-center flex-col justify-center w-full max-w-[800px] gap-8 p-8">
-        <div className="flex justify-center">
-          <FontAwesomeIcon icon={faCloud} className="w-[100px]" />
-        </div>
-        <h1 className="text-3xl font-bold text-red-50">Diego Roccia</h1>
-        <Button variant="link">Enter</Button>
-        <Footer />
-      </main>
+    <main className="container mx-auto text-slate-400 bg-slate-900 min-h-[100dvh] flex items-center flex-col justify-center w-full max-w-[800px] gap-8 p-4 md:p-8">
+      <div className="flex justify-center">
+        <FontAwesomeIcon icon={faCloud} className="w-[100px]" />
+      </div>
+      <h1 className="text-3xl font-bold text-red-50">Diego Roccia</h1>
+      <Button variant="link">Enter</Button>
+      <Footer />
+    </main>
   );
 }
