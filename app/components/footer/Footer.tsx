6a84ed80035b310b6d885da8420d629713c5c63d
Diego Roccia <diego.roccia@gmail.com>
2025-01-18 01:19:57 +0100

    Sat Jan 18 01:19:57 AM CET 2025
    Signed-off-by: Diego Roccia <diego.roccia@gmail.com>

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,29 +1,35 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { IconName, library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 
 library.add(fab);
 
+const socialLinks: { network: IconName; link: string }[] = [
+  { network: "telegram", link: "mailto:diego.roccia@gmail.com" },
+  { network: "linkedin", link: "https://www.linkedin.com/in/diegoroccia" },
+  { network: "github", link: "https://www.github.com/diegoroccia" },
+  { network: "facebook", link: "https://www.facebook.com/diego.roccia" },
+  { network: "instagram", link: "https://www.instagram.com/diegoroccia" },
+];
+
 export default function Footer() {
   return (
-    <footer className="flex-auto w-full text-slate-600 absolute bottom-4 pb-4 pr-4">
+    <footer className="flex-auto w-full text-slate-600 fixed bottom-0 p-4">
       <div className="flex justify-end space-x-2">
-        <a
-          href="https://github.com/diegoroccia"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="GitHub"
-        >
-          <FontAwesomeIcon icon={["fab", "github"]} className=" w-4 h-4" />
-        </a>
-        <a
-          href="https://linkedin.com/in/diegoroccia"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="LinkedIn"
-        >
-          <FontAwesomeIcon icon={["fab", "linkedin"]} className="w-4 s-4" />
-        </a>
+        {socialLinks.map((social) => (
+          <a
+            key={social.network}
+            href={social.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={social.network}
+          >
+            <FontAwesomeIcon
+              icon={["fab", social.network as IconName]}
+              className="w-4 h-4"
+            />
+          </a>
+        ))}
       </div>
     </footer>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="antialiased text-slate-400 bg-slate-900 min-h-screen flex flex-col items-center justify-center">
-      <main className="w-full max-w-[800px] flex flex-col items-center gap-8 px-4">
+      <main className="container mx-auto text-slate-400 bg-slate-900 min-h-screen flex items-center flex-col justify-center w-full max-w-[800px] gap-8 p-8">
         <div className="flex justify-center">
           <FontAwesomeIcon icon={faCloud} className="w-[100px]" />
         </div>
-        <h1 className="text-3xl font-bold">Diego Roccia</h1>
+        <h1 className="text-3xl font-bold text-red-50">Diego Roccia</h1>
         <Button variant="link">Enter</Button>
         <Footer />
       </main>
-    </div>
   );
 }
