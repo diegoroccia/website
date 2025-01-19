3869e12a43281039a9d0b824ae415d34a6e0ca11
Diego Roccia <diego.roccia@gmail.com>
2025-01-19 15:10:01 +0100

    Sun Jan 19 03:10:01 PM CET 2025
    Signed-off-by: Diego Roccia <diego.roccia@gmail.com>

---
+++ app/lib/posts.ts
@@ -0,0 +1,31 @@
+import { glob } from "glob";
+import matter from "gray-matter";
+
+export async function getAllBlogPosts() {
+  // getting all .md files from the posts directory
+  const blogPosts = glob.sync(`posts/**/*.md`);
+
+  const blogData = await Promise.all(
+    blogPosts.map(async (file) => {
+      const blogPostSlug = file
+        .split("/")[1]
+        .replace(/ /g, "-")
+        .slice(0, -3)
+        .trim();
+      const blogPostContent = await import(`../../posts/${file}`);
+      const data = matter(blogPostContent);
+
+      return {
+        slug: blogPostSlug,
+        path: file,
+        content: blogPostContent,
+        frontmatter: data.data,
+      };
+    }),
+  );
+
+  return {
+    blogData,
+    fallback: false,
+  };
+}
