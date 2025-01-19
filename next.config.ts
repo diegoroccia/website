830914cb7d260cbb9dd8a9c1bf30c876368f3c55
Diego Roccia <diego.roccia@gmail.com>
2025-01-19 15:12:41 +0100

    Sun Jan 19 03:12:41 PM CET 2025
    Signed-off-by: Diego Roccia <diego.roccia@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  pageExtensions: ["js", "jsx", "md", "ts", "tsx"],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      loaders: {
+        ".md": ["raw-loader"],
+      },
+    },
+  },
+};
 
 export default nextConfig;
